4e73629e01b7fc7c805c185c49cbc7cd97da8dc7
Seth Keating <seth.keating@gmail.com>
2022-05-26 20:34:59 -0400

    feat: implement sessions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,54 @@
 const router = require('express').Router();
+const { Workout, Entry, Exercise } = require('../models');
 
+router.get('/', (req, res) => {
+  res.render('feed', { loggedIn: req.session.loggedIn });
+});
+
+router.get('/dashboard', (req, res) => {
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+    return;
+  }
+
+  Workout.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    include: [
+      {
+        model: Entry,
+        attributes: ['set_count', 'rep_count', 'weight'],
+        include: {
+          model: Exercise,
+          attributes: ['exercise_name']
+        }
+      }
+    ]
+  })
+    .then(dbWorkoutData => {
+      console.log(dbWorkoutData.map(workout => workout.dataValues));
+      res.render('dashboard', 
+      { 
+        greeting: req.session.username,
+        loggedIn: req.session.loggedIn,
+        posts: dbWorkoutData.map(workout => workout.get({ plain: true }))
+      })
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+
+  // res.render('dashboard',
+  // { 
+  //   user_id: req.session.user_id 
+  // });
+});
+
+router.get('/dashboard/new', (req, res) => {
+
+});
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
@@ -9,5 +58,15 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
 
 module.exports = router;
