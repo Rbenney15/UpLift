e30af71cd858574e1f7c9aabe7c2cd5780c687b2
Seth Keating <seth.keating@gmail.com>
2022-05-30 10:46:22 -0400

    feat: add-workout page with basic form; needs post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Workout, Category, Exercise, Entry } = require('../models');
+const { Workout, Exercise, Entry } = require('../models');
 
 // dashboard/ - Return latest 5 workouts
 router.get('/', (req, res) => {
@@ -75,33 +75,6 @@ router.get('/all', (req, res) => {
         loggedIn: req.session.loggedIn,
         all: true,
         posts: dbWorkoutData.map(workout => workout.get({ plain: true }))
-      })
-    })
-    .catch(err => {
-      console.error(err);
-      res.sendStatus(500);
-    })
-});
-
-// dashboard/new - Make a new post; must expose available exercises to choose from to create add-workout
-router.get('/new', (req, res) => {
-  if (!req.session.loggedIn) {
-    res.redirect('/login');
-  }
-
-  Category.findAll({
-    include: {
-      model: Exercise,
-      attributes: ['exercise_name']
-    }
-  })
-    .then(dbCatData => {
-      res.render('add-workout',
-      {
-        loggedIn: req.session.loggedIn,
-        greeting: req.session.username,
-        user_id: req.session.user_id,
-        categories: dbCatData.map(category => category.get({ plain: true }))
       })
     })
     .catch(err => {
