3837c3ad893374ab36ae329e21c516d6c796e00a
Rob Benney <rob.benney@gmail.com>
2023-04-16 17:33:34 -0400

    Comments on server file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// import (npm) pacakges and modules
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
@@ -8,6 +9,7 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Setup a Sequilize connection to a database and a session store for storing user data
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -23,9 +25,11 @@ const sess = {
 
 app.use(session(sess));
 
+// Setup Handlebars template engine and configures it for the server
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Sets up middleware to parse request bodies and to serve static files
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
